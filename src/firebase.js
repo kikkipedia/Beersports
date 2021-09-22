4be145ab2e8a40e88a42097840f7d77d714b52fb
kikkipedia <kristinakerlund@gmail.com>
2021-09-22 16:03:24 +0200

    added firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,17 @@
-// se https://blog.logrocket.com/user-authentication-firebase-react-apps/
+import * as firebase from 'firebase'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID 
+}
+//initialize firebase and firestore to the app
+firebase.initializeApp(firebaseConfig)
+firebase.firestore()
+
+export default firebase
